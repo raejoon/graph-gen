64a69fc19a758332dfccb25d7e23ed8234094fc1
Raejoon Jung <raejoon@stanford.edu>
2019-09-30 14:10:36 -0700

    analyze: Moved to simulate.

--- graph-simulate/analyze.py
+++ graph-simulate/analyze.py
@@ -2,13 +2,16 @@
 
 """
 Usage:
-    python main.py --logdir DIR --min-broadcast-count --outfile FILE
-    python main.py --logdir DIR --converge-time --outfile FILE
-    python main.py --logdir DIR --deficit --transient --outfile FILE
+    python analyze.py --logdir DIR --min-broadcast-count --outfile FILE
+    python analyze.py --logdir DIR --converge-time --outfile FILE
+    python analyze.py --logdir DIR --converge-time --cdf
+    python analyze.py --logdir DIR --deficit --transient --outfile FILE
 """
 
 import argparse
 import os
+import numpy as np
+from constants import INTERVAL, SIMULATION_DURATION
 
 def examine_min_broadcast_count(logfile):
     broadcasts = {}
@@ -24,10 +27,42 @@ def examine_min_broadcast_count(logfile):
 
 
 def examine_converge_time(logfile):
-    pass
+    broadcasts = {}
+    with open(logfile) as fo:
+        for line in fo:
+            if "init" in line:
+                node_id = int(line.split(",")[1])
+                broadcasts[node_id] = []
+            elif "broadcast" in line:
+                values = line.split(",")
+                timestamp = int(values[0])
+                node_id = int(values[1])
+                broadcasts[node_id].append(timestamp)
+    
+    max_time = float("-inf")
+    for node_id in broadcasts:
+        error = np.abs(np.diff(broadcasts[node_id]) - INTERVAL) / INTERVAL
+        for ind in range(len(error) - 1, -1, -1):
+            if error[ind] > 1e-6:
+                break
+        converge_time = broadcasts[node_id][ind + 1]
+        if ind == len(error) - 1:
+            converge_time = float("inf")
+        if max_time < converge_time:
+            max_time = converge_time
+    
+    return max_time
+
 
 def examine_transient_deficit(logfile):
     pass
+
+
+def calculate_cdf(data, range_min, range_max, nbins):
+    bins = np.linspace(range_min, range_max, nbins + 1)
+    hist, bin_edges = np.histogram(data, bins)
+    cdf = np.cumsum(hist) / len(data)
+    return bin_edges[1:], cdf
 
 
 def read_file_list(logdir):
@@ -42,8 +77,10 @@ if __name__ == "__main__":
                 description="Analyze simulation logs.")
     parser.add_argument("--logdir", required=True,
                         help="Directory containing simulation logs")
-    parser.add_argument("--outfile", required=True,
+    parser.add_argument("--outfile",
                         help="Output file")
+    parser.add_argument("--cdf", action="store_true",
+                        help="Flag to present the distribution of the stat")
     
     stat_group = parser.add_mutually_exclusive_group(required=True)
     stat_group.add_argument("--min-broadcast-count", action="store_true",
@@ -57,6 +94,8 @@ if __name__ == "__main__":
                         help="Collect deficits only in transient phase")
     
     args = parser.parse_args()
+    if args.converge_time is None and args.cdf is not None:
+        parser.error("--cdf is only used with --converge-time.")
     if args.deficit is None and args.transient is not None:
         parser.error("--transient is only used with --deficit.")
     if args.deficit is not None and args.transient is None:
@@ -71,9 +110,19 @@ if __name__ == "__main__":
 
     if args.converge_time:
         data = [examine_converge_time(f) for f in filepath_list]
-    
+
     if args.deficit and args.transient:
         data = [examine_transient_deficit(f) for f in filepath_list]
     
-    with open(args.outfile, "w") as fo:
-        fo.write("\n".join(str(d) for d in data) + "\n")
+    if args.cdf:
+        bins, cdfs = calculate_cdf(data, 0, SIMULATION_DURATION, 20)
+        output_str = "\n".join("%f\t%f" % (b, c) for (b, c) in zip(bins, cdfs))
+    else:
+        output_str = "\n".join("%s\t%s" % (f, str(d))
+                                    for f, d in zip(filepath_list, data))
+    
+    if args.outfile:
+        with open(args.outfile, "w") as fo:
+            fo.write(output_str + "\n")
+    else:
+        print(output_str)
