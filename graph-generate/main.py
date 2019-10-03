4138accbd934a5323b1c5a6c1f19d300eda51187
Raejoon Jung <raejoon@stanford.edu>
2019-10-02 19:18:57 -0700

    generate: Added powers option for complete graphs.
    If powers option is enabled, complete graphs of size of powers of
    2 is generated.

--- graph-generate/main.py
+++ graph-generate/main.py
@@ -5,14 +5,25 @@ import networkx as nx
 
 """
 Usage:
-    python3 ./main.py --complete --max 6 --outdir DIR
+    python3 ./main.py --complete --max 6 --powers --outdir DIR
     python3 ./main.py --small --max 6 --outdir DIR
     python3 ./main.py --udg --dim 100 --num 200 --max-seed 10 --outdir DIR
     python3 ./main.py --binomial --prob 0.5 --num 200 --max-seed 10 --outdir DIR
 """
 
-def generate_complete_graphs(max_size):
-    return [nx.complete_graph(n) for n in range(2, max_size + 1)] 
+def generate_complete_graphs(max_size, flag_powers):
+    if not flag_powers:
+        return [nx.complete_graph(n) for n in range(2, max_size + 1)] 
+    else:
+        graphs = []
+        exponent = 1
+        while True:
+            n = 2**exponent
+            if n > max_size:
+                break
+            graphs.append(nx.complete_graph(n))
+            exponent +=1
+        return graphs
 
 
 def generate_small_graphs(max_size):
@@ -68,21 +79,23 @@ if __name__=="__main__":
                             help="Flag to generate Erdos-Renyi graph")
 
     parser.add_argument("--outdir", required=True,
-                       help="Output directory")
+                        help="Output directory")
     # geometric graph attributes
     parser.add_argument("--dim", type=int,
-                       help="Size of the grid for UDG placement")
+                        help="Size of the grid for UDG placement")
     # random graph attributes
     parser.add_argument("--prob", type=float,
-                       help="Link probability for Erdos-Renyi graphs") 
+                        help="Link probability for Erdos-Renyi graphs") 
     # deterministically generated graphs
     parser.add_argument("--max", type=int,
-                       help="Maximum number of nodes for complete/small")
+                        help="Maximum number of nodes for complete/small")
+    parser.add_argument("--powers", action="store_true",
+                        help="Number of nodes in powers of 2 for complete") 
     # randomly generated graphs
     parser.add_argument("--num", type=int,
-                       help="Number of nodes for randomly generated graphs")
+                        help="Number of nodes for randomly generated graphs")
     parser.add_argument("--max-seed", type=int,
-                       help="Max seed value for randomly generated graphs")
+                        help="Max seed value for randomly generated graphs")
 
     args = parser.parse_args()
     if not os.path.isdir(args.outdir):
@@ -96,9 +109,11 @@ if __name__=="__main__":
         parser.error("--udg requires --dim, --num, and --max-seed.")
     if args.binomial and (None in [args.prob, args.num, args.max_seed]):
         parser.error("--random requires --prob, --num, --max-seed.")
+    if args.small and args.powers:
+        parser.error("--powers cannot be used with --small.")
 
     if args.complete:
-        graph_list = generate_complete_graphs(args.max)
+        graph_list = generate_complete_graphs(args.max, args.powers)
     elif args.small:
         graph_list = generate_small_graphs(args.max)
     elif args.udg: 
