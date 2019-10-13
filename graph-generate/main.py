59315adc08b01cc6cc8b99e57a8918719bfb3b0d
Raejoon Jung <raejoon@stanford.edu>
2019-10-13 16:51:17 -0700

    generate: Two major changes.
    1. UDG assumes 2d grid.
    2. Random graphs are generated in parallel.

--- graph-generate/main.py
+++ graph-generate/main.py
@@ -8,8 +8,9 @@ import multiprocessing as mp
 Usage:
     python3 ./main.py --complete --max 6 --powers --outdir DIR
     python3 ./main.py --small --max 6 --outdir DIR
-    python3 ./main.py --udg --dim 100 --num 200 --max-seed 10 --outdir DIR
-    python3 ./main.py --binomial --prob 0.5 --num 200 --max-seed 10 --outdir DIR
+    python3 ./main.py --udg --radius 1 --num 200 --max-seed 10 --outdir DIR
+    python3 ./main.py --binomial --prob 0.5 --num 200 --max-seed 5 \
+            --outdir DIR --parallel 4
 """
 
 def generate_complete_graphs(max_size, flag_powers):
@@ -32,58 +33,36 @@ def generate_small_graphs(max_size):
                 if len(g) > 1 and len(g) <= max_size and nx.is_connected(g)]
 
 
-def generate_udg_graphs(dim, num, max_seed):
-    graph_list = [None for _ in range(max_seed + 1)]
-    for seed in range(max_seed + 1):
-        graph = nx.random_geometric_graph(num, 1, dim=dim, seed=seed)
-        graph_list[seed] = graph
-    return graph_list
+def generate_udg_graph(parameters):
+    radius = parameters[0] 
+    num = parameters[1]
+    seed = parameters[2]
+    return nx.random_geometric_graph(num, radius, seed=seed)
 
 
-def generate_udg_graph(dim, num, seed):
-    return nx.random_geometric_graph(num, 1, dim=dim, seed=seed)
+def generate_binomial_graph(parameters):
+    prob = parameters[0]
+    num = parameters[1]
+    seed = parameters[2]
+    return nx.gnp_random_graph(num, prob, seed=seed)
 
 
-def generate_udg_graphs_parallel(dim, num, max_seed):
-    graph_list = [None for _ in range(max_seed + 1)]
-    results = []
-    with mp.Pool(processes=8) as pool:
-        for seed in range(max_seed + 1):
-            func = generate_udg_graph
-            args = (dim, num, seed,)
-            results.append(pool.apply_async(func, args))
-
-        for seed in range(max_seed + 1):
-            graph_list[seed] = results[seed].get()
-
-    return graph_list
-
-        
-def generate_binomial_graphs(prob, num, max_seed):
-    graph_list = [None for _ in range(max_seed + 1)]
-    for seed in range(max_seed + 1):
-        graph = nx.gnp_random_graph(num, prob, seed=seed)
-        graph_list[seed] = graph
-    return graph_list
+def generate_graphs_from_args(func, args_list, nproc):
+    if nproc == 1:
+        return [func(args) for args in args_list]
+    
+    with mp.Pool(processes=nproc) as pool:
+        return pool.map(func, args_list)
 
 
-def generate_binomial_graph(prob, num, seed):
-    return nx.gnp.random_graph(num, prob, seed=seed)
+def generate_udg_graphs(radius, num, max_seed, nproc):
+    args_list = [(radius, num, seed,) for seed in range(max_seed + 1)]
+    return generate_graphs_from_args(generate_udg_graph, args_list, nproc)
 
 
-def generate_binomial_graphs_parallel(prob, num, max_seed):
-    graph_list = [None for _ in range(max_seed + 1)]
-    results = []
-    with mp.Pool(processes=8) as pool:
-        for seed in range(max_seed + 1):
-            func = generate_binomial_graph
-            args = (num, prob, seed,)
-            results.append(pool.apply_async(func, args))
-
-        for seed in range(max_seed + 1):
-            graph_list[seed] = results[seed].get()
-
-    return graph_list
+def generate_binomial_graphs(prob, num, max_seed, nproc):
+    args_list = [(prob, num, seed,) for seed in range(max_seed + 1)]
+    return generate_graphs_from_args(generate_binomial_graph, args_list, nproc)
 
 
 def save_graph_list(graph_list, parameters, outdir):
@@ -120,8 +99,8 @@ if __name__=="__main__":
     parser.add_argument("--outdir", required=True,
                         help="Output directory")
     # geometric graph attributes
-    parser.add_argument("--dim", type=int,
-                        help="Size of the grid for UDG placement")
+    parser.add_argument("--radius", type=float,
+                        help="Distance threshold for UDG placement")
     # random graph attributes
     parser.add_argument("--prob", type=float,
                         help="Link probability for Erdos-Renyi graphs") 
@@ -135,6 +114,8 @@ if __name__=="__main__":
                         help="Number of nodes for randomly generated graphs")
     parser.add_argument("--max-seed", type=int,
                         help="Max seed value for randomly generated graphs")
+    parser.add_argument("--parallel", type=int, default=1,
+                        help="Number of processes to use (default: 1)")
 
     args = parser.parse_args()
     args.outdir = os.path.normpath(args.outdir)
@@ -145,23 +126,25 @@ if __name__=="__main__":
     
     if args.small and (args.max is None):
         parser.error("--small requires --max.") 
-    if args.udg and (None in [args.dim, args.num, args.max_seed]):
-        parser.error("--udg requires --dim, --num, and --max-seed.")
+    if args.udg and (None in [args.num, args.max_seed, args.radius]):
+        parser.error("--udg requires --num, and --max-seed.")
     if args.binomial and (None in [args.prob, args.num, args.max_seed]):
-        parser.error("--random requires --prob, --num, --max-seed.")
+        parser.error("--binomial requires --prob, --num, --max-seed.")
     if args.small and args.powers:
         parser.error("--powers cannot be used with --small.")
+    if any([args.small, args.complete]) and args.parallel is not None:
+        print("--parallel is ignored with --complete or --small")
 
     if args.complete:
         graph_list = generate_complete_graphs(args.max, args.powers)
     elif args.small:
         graph_list = generate_small_graphs(args.max)
     elif args.udg: 
-        graph_list = generate_udg_graphs_parallel(args.dim, args.num, 
-                                                  args.max_seed)
+        graph_list = generate_udg_graphs(args.radius, args.num, args.max_seed,
+                                         args.parallel)
     elif args.binomial:
-        graph_list = generate_binomial_graphs_parallel(args.prob, args.num, 
-                                                       args.max_seed)
+        graph_list = generate_binomial_graphs(args.prob, args.num, 
+                                              args.max_seed, args.parallel)
     
     parameters = " ".join(sys.argv[1:])
     save_graph_list(graph_list, parameters, args.outdir)
